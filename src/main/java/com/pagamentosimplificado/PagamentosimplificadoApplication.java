cddc61ae91dfcf7a15450a0090e94fd6a608590b
magdielrocha <magdiel002@gmail.com>
2026-02-26 09:16:39 -0300

    feat: initial config

---
+++ src/main/java/com/pagamentosimplificado/PagamentosimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.pagamentosimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosimplificadoApplication.class, args);
+	}
+
+}
