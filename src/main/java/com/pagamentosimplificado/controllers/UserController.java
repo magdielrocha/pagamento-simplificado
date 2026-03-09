a760c4f973cf5ad8347d1c44a35efd1cdf7f8a47
magdielrocha <magdiel002@gmail.com>
2026-03-09 17:22:27 -0300

    feat(user): add UserController

--- src/main/java/com/pagamentosimplificado/controllers/UserController.java
+++ src/main/java/com/pagamentosimplificado/controllers/UserController.java
@@ -1,4 +1,38 @@
 package com.pagamentosimplificado.controllers;
 
+import com.pagamentosimplificado.domain.user.User;
+import com.pagamentosimplificado.dtos.user.UserDTO;
+import com.pagamentosimplificado.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody @Valid UserDTO user) {
+
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/pagamentosimplificado/dtos/user/UserDTO.java
+++ src/main/java/com/pagamentosimplificado/dtos/user/UserDTO.java
@@ -1,4 +1,32 @@
 package com.pagamentosimplificado.dtos.user;
 
-public record UserDTO() {
+import com.pagamentosimplificado.domain.user.UserType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record UserDTO(
+        @NotBlank
+        String firstName,
+
+        @NotBlank
+        String lastName,
+
+        @NotBlank
+        String cnpjCpf,
+
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password,
+
+        BigDecimal balance,
+
+        @NotNull
+        UserType userType
+) {
 }
